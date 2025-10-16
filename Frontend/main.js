c6a85b6b3b27644819ba462efab759b99bee843f
nypblockchain <nypsitblockchain@gmail.com>
2025-10-16 11:15:20 +0800

    Timeless

--- Frontend/main.js
+++ Frontend/main.js
@@ -58,8 +58,8 @@ function renderMaze(maze, players = {}, pings = {}) {
     const mid = Math.floor(rows / 2); // find center
 
     mazeDiv.style.display = "grid";
-    mazeDiv.style.gridTemplateColumns = `repeat(${cols}, 40px)`;
-    mazeDiv.style.gridTemplateRows = `repeat(${rows}, 40px)`;
+    mazeDiv.style.gridTemplateColumns = `repeat(${cols}, 54px)`;
+    mazeDiv.style.gridTemplateRows = `repeat(${rows}, 54px)`;
 
     const now = Date.now();
     console.log("RenderMaze input:", { pings, players, now})
