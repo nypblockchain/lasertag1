539890bd5af84db7a394732487e47cc4694a67b2
nypblockchain <nypsitblockchain@gmail.com>
2025-10-28 11:56:54 +0800

    0

--- api/move.js
+++ api/move.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
         return res.status(405).json({ error: "Method not allowed" });
     }
 
-    const { direction, playerId } = req.body || {};
+    const { direction, playerId, respawn } = req.body || {};
 
     if (respawn) {
         try {
